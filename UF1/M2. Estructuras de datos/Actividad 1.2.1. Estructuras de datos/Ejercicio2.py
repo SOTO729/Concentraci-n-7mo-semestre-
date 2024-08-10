bfe0282c217d810ed9036b95208eff8d2a00a599
SOTO729 <154265732+SOTO729@users.noreply.github.com>
2024-08-10 22:17:05 +0000

    RELOAD

--- UF1/M2. Estructuras de datos/Actividad 1.2.1. Estructuras de datos/Ejercicio2.py
+++ UF1/M2. Estructuras de datos/Actividad 1.2.1. Estructuras de datos/Ejercicio2.py
@@ -10,5 +10,42 @@ EJERCICIO 2:
  Obtenga como salida una lista de los números ordenados (de menor a mayor), pero sin repeticiones.
 
 """
+print("""#### INSTRUCCIONES ####\n1. Insertar los elementos que desea agregar a la lista (deben ser menores a 10)\n
+      2. Ingresar elementos de la lista uno por uno.\n
+      3. Recuerde que no son validos los caracteres en secciones donde se le solicita un numero.\n""")
+
 vector=[]
-veri
+while True:
+        try:
+            N=input('Ingrese al numero de elementos que desea agregar\n')
+            if N.count('.')!=0:
+                N=float(N)
+                if N>10:
+                    1/0
+                else:
+                    print('Dado que solo se aceptan valores enteros, su registro "',N,'" sera agregado como: "',int(N),'".')
+                    N=int(N)
+                    break
+            else:
+                N=int(float(N))
+
+                if N>10:
+                    1/0                
+                break
+        except:
+            print('Valor ingresado inválido\n')
+
+for i in range(N):
+    while True:
+        try:
+            elemento=input('Ingrese el elemento del vector No.: '+str(i+1)+'   \n')
+            if elemento.count('.')!=0:
+                elemento=float(elemento)
+                print('Dado que solo se aceptan valores enteros, su registro "',elemento,'" sera modificado a "',int(elemento),'".\n')
+            elemento=int(elemento)
+            vector.append(elemento)
+            break
+        except:
+            print('Valor ingresado invalido, favor de intentarlo de nuevo.\n')
+vector=list(set(sorted(vector)))
+print(vector)
--- UF1/M2. Estructuras de datos/Actividad 1.2.1. Estructuras de datos/Ejercicio4.py
+++ UF1/M2. Estructuras de datos/Actividad 1.2.1. Estructuras de datos/Ejercicio4.py
@@ -7,3 +7,65 @@ EJERCICIO 4:
 
 ▹ A partir de este, crear 2 vectores; uno con los números pares y el otro con los numero impares, además, decir de los vectores cual es más grande y el número de elementos en cada vector.
 """
+print("""#### INSTRUCCIONES ####\n1. Insertar los elementos que desea agregar a la lista (deben ser menores a 10)\n
+      2. Ingresar elementos de la lista uno por uno.\n
+      3. Recuerde que no son validos los caracteres en secciones donde se le solicita un numero.\n""")
+
+vector=[]
+while True:
+        try:
+            N=input('Ingrese al numero de elementos que desea agregar\n')
+            if N.count('.')!=0:
+                N=float(N)
+                if N>10:
+                    1/0
+                else:
+                    print('Dado que solo se aceptan valores enteros, su registro "',N,'" sera agregado como: "',int(N),'".')
+                    N=int(N)
+                    break
+            else:
+                N=int(float(N))
+
+                if N>10:
+                    1/0                
+                break
+        except:
+            print('Valor ingresado inválido\n')
+
+for i in range(N):
+    while True:
+        try:
+            elemento=input('Ingrese el elemento del vector No.: '+str(i+1)+'   \n')
+            if elemento.count('.')!=0:
+                elemento=float(elemento)
+                print('Dado que solo se aceptan valores enteros, su registro "',elemento,'" sera modificado a "',int(elemento),'".\n')
+            elemento=int(elemento)
+            vector.append(elemento)
+            break
+        except:
+            print('Valor ingresado invalido, favor de intentarlo de nuevo.\n')
+vector=sorted(vector)
+pares,inpares=[],[]
+for i in vector:
+    if i%2==0:
+        pares.append(i)
+    else:
+        inpares.append(i)
+N_PARES=len(pares)
+N_INPARES=len(inpares)
+if N_PARES>N_INPARES:
+    print('El vector con los numeros pares es el más grande con',N_PARES,'elementos.\n')
+    print('Lista pares:',pares,'\n')
+    print('El vector con los numeros inpares tiene',N_INPARES,'elementos.\n')
+    print('Lista inpares:',inpares,'\n')
+elif N_PARES<N_INPARES:
+    print('El vector con los numeros inpares es el más grande con',N_INPARES,'elementos.\n')
+    print('Lista pares:',pares,'\n')
+    print('El vector con los numeros pares tiene',N_PARES,'elementos.\n')
+    print('Lista inpares:',inpares,'\n')
+else:
+    print('Ambos tienen el mismo tamaño:\n')
+    print('Pares:',N_PARES,'\n')
+    print('Lista pares:',pares,'\n')
+    print('Inpares:',N_INPARES,'\n')
+    print('Lista inpares:',inpares,'\n')
