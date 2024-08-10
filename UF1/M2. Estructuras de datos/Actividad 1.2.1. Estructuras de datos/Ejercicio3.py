bff37a442673fd545f3673aee1e4fc095462ba11
SOTO729 <154265732+SOTO729@users.noreply.github.com>
2024-08-10 22:47:06 +0000

    REFRESH

--- UF1/M2. Estructuras de datos/Actividad 1.2.1. Estructuras de datos/Ejercicio3.py
+++ UF1/M2. Estructuras de datos/Actividad 1.2.1. Estructuras de datos/Ejercicio3.py
@@ -11,17 +11,15 @@ EJERCICIO 3:
 """
 temperaturas=[]
 print('Ingrese todas las temperaturas que quiera\nCuando termine de ingresar las temperaturas digite "x"')
-while True:
-    try:
-        temperatura_actual=input('Ingrese su temperatura a registrar')
-        temperatura_actual=float(temperatura_actual)
-        temperaturas.append(temperatura_actual)
+while True:#Inicia ciclo while para verificar si el contenido ingresado por el usuario es correcto
+    try:#Inicia bloque try
+        temperatura_actual=input('Ingrese su temperatura a registrar')#Se pide la temperatura
+        temperatura_actual=float(temperatura_actual)#Se convierte a flotante, si es una cadena pasa al bloque except
+        temperaturas.append(temperatura_actual)#Si es un numero se añade a la lista temperaturas
     except:
-        if temperatura_actual.upper()=='X':
-            temperaturas.pop()
-            break
+        if temperatura_actual.upper()=='X':#Se verifica si se ingreso x para romper el ciclo
+            temperaturas.pop()#Si es asi se elimina la x de la lista temperaturas
+            break#Y se rompe el ciclo
         else:
-            print('Valor de temperatura invalido!!!\nFavor de escribir solo numeros...')
-print('La media de las temperaturas ingresadas es:',sum(temperaturas)/len(temperaturas))
-import numpy as np
-print('Resultado funcion de numpy:',np.mean(temperaturas))
+            print('Valor de temperatura invalido!!!\nFavor de escribir solo numeros...')#Mensaje de error
+print('La media de las temperaturas ingresadas es:',sum(temperaturas)/len(temperaturas))#Calculo de la media.
--- UF1/M2. Estructuras de datos/Actividad 1.2.1. Estructuras de datos/Ejercicio4.py
+++ UF1/M2. Estructuras de datos/Actividad 1.2.1. Estructuras de datos/Ejercicio4.py
@@ -11,59 +11,57 @@ print("""#### INSTRUCCIONES ####\n1. Insertar los elementos que desea agregar a 
       2. Ingresar elementos de la lista uno por uno.\n
       3. Recuerde que no son validos los caracteres en secciones donde se le solicita un numero.\n""")
 
-vector=[]
-while True:
-        try:
-            N=input('Ingrese al numero de elementos que desea agregar\n')
-            if N.count('.')!=0:
-                N=float(N)
-                if N>10:
+vector=[] #Se inicia la lista vector
+while True: #Inicia ciclo para pedir varias veces el numero de elementos que se desea que tenga el vector hasta que cumpla con las especificaciones.
+        try:#Inicia bloque try
+            N=input('Ingrese al numero de elementos que desea agregar\n')#Se obtiene el numero de elementos a agregar en forma de cadena
+            if N.count('.')!=0: #se verifica si existe un punto decimal, si existe significa que puede ser flotante o una cadena
+                N=float(N)#si esta seccion arroja error entonces es una cadena y se pasa al bloque except
+                if N>10:#si el dato ingresado es mayor a 10 se fierza el error dividiendo entre 0
                     1/0
                 else:
-                    print('Dado que solo se aceptan valores enteros, su registro "',N,'" sera agregado como: "',int(N),'".')
-                    N=int(N)
-                    break
+                    print('Dado que solo se aceptan valores enteros, su registro "',N,'" sera agregado como: "',int(N),'".')#se notifica que se eliminara la parte decimal del resultado
+                    N=int(N)#Se transforma a entero el valor
+                    break#Se rompe el ciclo while
             else:
-                N=int(float(N))
-
-                if N>10:
+                N=int(float(N))#significa que el dato ingresado siempre fue entero
+                if N>10: #Se vuelve a verificar si el dato ingresado es entero y si es asi se fuerza el error dividiendo entre 0
                     1/0                
-                break
+                break#se rompe el ciclo while
         except:
-            print('Valor ingresado inválido\n')
+            print('Valor ingresado inválido\n')#siempre que se llegue a error saldra este mensaje
 
-for i in range(N):
+for i in range(N):#inicia ciclo for que considera la longitud ingresada por el usuario, con un range para poder imprimir el numero de cada iteracion
     while True:
         try:
-            elemento=input('Ingrese el elemento del vector No.: '+str(i+1)+'   \n')
-            if elemento.count('.')!=0:
-                elemento=float(elemento)
-                print('Dado que solo se aceptan valores enteros, su registro "',elemento,'" sera modificado a "',int(elemento),'".\n')
-            elemento=int(elemento)
-            vector.append(elemento)
+            elemento=input('Ingrese el elemento del vector No.: '+str(i+1)+'   \n')#se pide en formato str el elemento del vector cuyo numero depende del iterador i
+            if elemento.count('.')!=0:#Se repite el mismo proceso que en el primer while para verificar si es flotante o cadena
+                elemento=float(elemento)#En caso de ser cadena se pasa al bloque except
+                print('Dado que solo se aceptan valores enteros, su registro "',elemento,'" sera modificado a "',int(elemento),'".\n')#Se notifica al usuario que su parte decimal sera eliminada
+            elemento=int(elemento)#Se convierte ba entero
+            vector.append(elemento)#Se añade a la lista vector
             break
         except:
-            print('Valor ingresado invalido, favor de intentarlo de nuevo.\n')
-vector=sorted(vector)
-pares,inpares=[],[]
+            print('Valor ingresado invalido, favor de intentarlo de nuevo.\n')#Mensaje de error de este bloque except
+pares,inpares=[],[]#Se definen las listas pares e inpares como listas vacias.
 for i in vector:
-    if i%2==0:
+    if i%2==0:#se verifica si el residuo de dividir entre dos cada elemento de la lista principal tiene residuo cero para saber si es par o no y se añade en pares o inpares respectivamente.
         pares.append(i)
     else:
         inpares.append(i)
-N_PARES=len(pares)
-N_INPARES=len(inpares)
-if N_PARES>N_INPARES:
+N_PARES=len(pares)#Se obtiene el numero de elementos pares
+N_INPARES=len(inpares)#Se obtiene el numero de elementos inpares
+if N_PARES>N_INPARES:#Se verifica si pares es mayor que inpares y se imprimien los resultados
     print('El vector con los numeros pares es el más grande con',N_PARES,'elementos.\n')
     print('Lista pares:',pares,'\n')
     print('El vector con los numeros inpares tiene',N_INPARES,'elementos.\n')
     print('Lista inpares:',inpares,'\n')
-elif N_PARES<N_INPARES:
+elif N_PARES<N_INPARES:#Se verifica si inpares es mayor que pares y se imprimien los resultados
     print('El vector con los numeros inpares es el más grande con',N_INPARES,'elementos.\n')
     print('Lista pares:',pares,'\n')
     print('El vector con los numeros pares tiene',N_PARES,'elementos.\n')
     print('Lista inpares:',inpares,'\n')
-else:
+else:#Se activa cuando ambas listas son iguales
     print('Ambos tienen el mismo tamaño:\n')
     print('Pares:',N_PARES,'\n')
     print('Lista pares:',pares,'\n')
