69ca11677b33e2910b9097e7e35710298ffeb1bf
SOTO729 <154265732+SOTO729@users.noreply.github.com>
2024-09-04 04:08:32 +0000

    REFRESH

--- UF1/tarea ej3.py
+++ UF1/tarea ej3.py
@@ -1,2 +1,11 @@
-with open('comentarios.txt','r') as archivo:
-    print(archivo.readlines())
+import re
+with open('./UF1/comentarios.txt','r',encoding='utf-8') as archivo:
+    lineas=[]
+    for linea in archivo.readlines():
+        lineas.append(linea)
+    contenido=archivo.read()
+print(contenido)
+lista_patrones=['excelente','malo','problema','genial','defectuoso']
+for i in lista_patrones:
+    for j in lineas:
+        patron=r'\b'+lista_patrones[i]+'?\b'
