e8f0caa95ea288e973e489728f580d3eef854733
Diego Soto <154265732+SOTO729@users.noreply.github.com>
2024-11-22 21:51:15 +0000

    d

--- RETO/config.py
+++ RETO/config.py
@@ -1,4 +1,11 @@
-@st.cache_data
-def load_df():
-    df=1
-    return df
+#@st.cache_data
+def load_df(arg):
+    import pandas as pd
+    import datetime
+    data=pd.read_csv(arg)
+    data.dropna(inplace=True)
+    data['Tiempo']=data.index*3
+    data.dropna(inplace=True)
+    data['Tiempo']=data['Tiempo'].apply(lambda x: datetime.timedelta(seconds=x).total_seconds() / 60)
+    return data
+print(load_df('03may.csv'))
